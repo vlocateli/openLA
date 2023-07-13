ab0c40e8072f68145facb200a7159e805154f407
vlocateli <victorlocateli999@gmail.com>
2023-07-12 23:39:59 -0300

    SUBJECT: Added test for is_close, is_equal and...
    Added tests for is_close, is_equal and run_tests.

--- include/test.hh
+++ include/test.hh
@@ -1,24 +1,26 @@
 #ifndef TEST_HH
 #define TEST_HH
-#include"../include/matrix.hpp"
+//#include"../include/matrix.hpp"
 #include "../include/vector.hh"
+#include"../include/matrix.hpp"
+#include"../include/types.hh"
 #include<iostream>
 #include<chrono>
 #if __cplusplus <= 199711L
   #error This library needs at least a C++11 compliant compiler
 #endif
 
-
-extern void test_vector1()
+namespace test{
+void test_vector1()
 {
 	// testing constructors
-	openLA::Vector<size_t> v1; // without arguments
-	openLA::Vector<size_t> v2(10); // size only
-	openLA::Vector<size_t> v3(10,0); // size and initial value
-	openLA::Vector<size_t> v4(10,std::initializer_list<size_t>{1,2,3,4,5
+	openLA::Vector<usize> v1; // without arguments
+	openLA::Vector<usize> v2(10); // size only
+	openLA::Vector<usize> v3(10,0); // size and initial value
+	openLA::Vector<usize> v4(10,std::initializer_list<usize>{1,2,3,4,5
 															   ,6,7,8,9,10});
 	//size and initializer list
-	openLA::dimension2::Matrix<size_t> m1(10,10,0);
+	openLA::dimension2::Matrix<usize> m1(10,10,0);
 	
 	assert(v1.capacity() == 0);
 	assert(v2.capacity() == 10);
@@ -33,18 +35,19 @@ extern void test_vector1()
 	std::cout << m1 << '\n';
 }
 
-extern void test_matrix1()
+void test_matrix1()
 {
-	openLA::dimension2::Matrix<size_t> m1(3,3,0);
+	openLA::dimension2::Matrix<usize> m1(3,3,0);
 	std::cout << "m1\n";
 	std::cout << m1 << "\n";
 }
-extern void test_matrix2()
+
+void test_matrix2()
 {
-	openLA::dimension2::Matrix<size_t> m1(3,3,{1,2,3,
+	openLA::dimension2::Matrix<usize> m1(3,3,{1,2,3,
 											   4,5,6,
 											   7,8,9});
-	openLA::dimension2::Matrix<size_t> m2(3,3,{1,2,3,
+	openLA::dimension2::Matrix<usize> m2(3,3,{1,2,3,
 											   4,5,6,
 											   7,8,9});
 
@@ -59,18 +62,115 @@ extern void test_matrix2()
 	std::cout << "testing operator *\n";
 	std::cout << m1 * m2 << '\n';
 }
-extern void test_matrix3()
+
+void test_matrix3()
 {
 	using namespace openLA::dimension2;
-	using T = size_t;
-	Matrix<T> m (1024,1024,0);
-	Matrix<T> n (1024,1024,0);
+	static constexpr auto ROWS = 1024;
+	static constexpr auto COLUMNS = 1024; 
+	using T = usize;
+	Matrix<T> m (ROWS,COLUMNS,0);
+	Matrix<T> n (ROWS,COLUMNS,0);
 	m.random_fill(0,1025);
 	n.random_fill(0,1025);
 	const auto start = std::chrono::steady_clock::now();
-	auto r = m * n;
+	const auto r1 = m * n;
 	const auto end = std::chrono::steady_clock::now();
-    const std::chrono::duration<double> elapsed_seconds = end - start;	
-	std::cout << "elapsed time: "<< elapsed_seconds.count()  << "s"<< '\n';
+    const std::chrono::duration<f64> elapsed_seconds = end - start;	
+	std::cout << "elapsed time "<< elapsed_seconds.count()  << "s"<< '\n';
 }
+
+void test_is_close1()
+{
+	using namespace openLA::dimension2;
+	using T = s32;
+	static constexpr usize ROWS = 1;
+	static constexpr usize COLUMNS = 5;
+	static constexpr auto EPSILON_D  = 0.008;
+	static constexpr auto EPSILON_F = 0.008f;
+	Matrix<T> m (ROWS,COLUMNS,{1,2,3,4,5});
+	Matrix<T> n (ROWS,COLUMNS,{10,20,30,40,50});
+	T tmp;
+	(void) tmp;
+	if(m.is_close(n,EPSILON_D)){
+		std::cout << "m is close matrix n\nDOUBLE\n";
+	}
+	else{
+		std::cout << "m is not close matrix n\nDOUBLE\n";
+		
+	}
+	if(m.is_close(n,EPSILON_F)){
+		std::cout << "m is close to matrix n\n FLOAT\n";
+	}
+	else{
+		std::cout << "m is not close matrix n\nFLOAT\n";
+	}
+}
+
+void test_is_close2()
+{
+	using namespace openLA::dimension2;
+	using T = s32;
+	static constexpr usize ROWS = 1;
+	static constexpr usize COLUMNS = 5;
+	static constexpr auto EPSILON_D  = 100.5;
+	static constexpr auto EPSILON_F = 100.5f;
+	Matrix<T> m (ROWS,COLUMNS,{1,2,3,4,5});
+	Matrix<T> n (ROWS,COLUMNS,{10,20,30,40,50});
+	T tmp;
+	(void) tmp;
+	if(m.is_close(n,EPSILON_D)){
+		std::cout << "m is close matrix n\nDOUBLE\n";
+	}
+	else{
+		std::cout << "m is not close matrix n\nDOUBLE\n";
+		
+	}
+	if(m.is_close(n,EPSILON_F)){
+		std::cout << "m is close to matrix n\n FLOAT\n";
+	}
+	else{
+		std::cout << "m is not close matrix n\nFLOAT\n";
+	}
+}
+
+void test_operator_is_equal()
+{
+	using namespace openLA::dimension2;
+	using T = s32;
+	static constexpr usize ROWS = 1;
+	static constexpr usize COLUMNS = 5;
+	Matrix<T> m(ROWS,COLUMNS,{1,2,3,4,5});
+	Matrix<T> n(ROWS,COLUMNS,{1,2,3,4,5});
+	openLA::Vector<T> v(5,{1,2,3,4,5});
+	assert(m == v);
+	assert(m == n );
+}
+
+void test_operator_is_equal2()
+{
+	using namespace openLA::dimension2;
+	using T = s32;
+	static constexpr usize ROWS = 2;
+	static constexpr usize COLUMNS = 5;
+	Matrix<T> m(ROWS,COLUMNS,{1,2,3,4,5,6,7,8,9,10});
+	Matrix<T> n(ROWS,COLUMNS,{1,2,3,4,5,7,8,9,10});
+	openLA::Vector<T> v(5,{1,2,3,4,5});
+	//assert(m == v);
+	//assert(m == n );
+	
+}
+void run_tests()
+{
+	test::test_vector1();
+	test::test_matrix1();
+	test::test_matrix2();
+	test::test_matrix3();
+	test::test_is_close1();
+	test::test_is_close2();
+	test::test_operator_is_equal();
+	test::test_operator_is_equal2();
+	
+}
+} // test
 #endif // TEST_HH
