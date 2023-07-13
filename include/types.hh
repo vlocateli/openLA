cc4be900524ca4a29425593c8431d4f6d533a32e
vlocateli <victorlocateli999@gmail.com>
2023-07-12 23:44:11 -0300

    SUBJECT: Added types header to make typing faster.

---
+++ include/types.hh
@@ -0,0 +1,19 @@
+#ifndef TYPES_HH
+#define TYPE_HH
+#include<iostream>
+#include<cstdint>
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using s8 = std::int8_t;
+using s16 = std::int16_t;
+using s32 = std::int32_t;
+using s64 = std::int64_t;
+
+using f32 = float;
+using f64 = double;
+using usize = std::size_t;
+#endif //TYPES_HH
+
