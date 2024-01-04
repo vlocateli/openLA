b31d2306c2a59ee4861d51464efb70360ba701ff
vlocateli <victorlocateli999@gmail.com>
2024-01-04 12:10:16 -0300

    Added operator +,-,*,<< and function resize
    Added also, cbegin, cend, begin and end.

--- include/vector.hh
+++ include/vector.hh
@@ -1,20 +1,23 @@
 #ifndef VECTOR_HH
 #define VECTOR_HH 1
-#pragma once
 #include <memory>
 #include <iostream>
+#include <cstring>
 #include "types.hh"
+#include "utilities.hh"
 namespace openLA{
 template<class T>
-    class vector{
+    class Vector{
         public:
-            std::shared_ptr<T> m_vector;
+            std::shared_ptr<T [] > m_vector;
             size_t m_total_elements;
-            vector<T>(size_t size)
+            using iterator = std::shared_ptr<T []>;
+            using const_iterator = const std::shared_ptr<T []>;
+            Vector<T>(usize size)
             : m_total_elements {size}
             {
                 try{
-                    m_vector =  std::make_unique<T>(size);
+                    m_vector =  std::make_unique<T []>(size);
                 }catch(std::bad_alloc())
                 {
                     throw std::runtime_error("Couldn't allocate memory\n");
@@ -23,30 +26,120 @@ template<class T>
                     std::cerr << "Unhandled exception\n";
                 }
             }
-            vector<T>()
-            : vector<T>(0)
+            Vector<T>()
+            : Vector<T>(0)
             {
             }
-            T operator[] (const usize index);
-    };
-}
-template<class T>
-T openLA::vector<T>::operator[] ( const usize index)
-{
-    return *(m_vector + index);
-}
-template<class T>
-T dot_product (const openLA::vector<T> &p, const openLA::vector<T> &q)
-{
-    if(p.m_total_elements != q.m_total_elements){
-        throw std::logic_error("ERROR: vectors have diferent\
-                lenght!\n");
-    }
-    T sum{};
-    for(usize i = 0; i < p.m_total_elements; i++){
-       sum += p.m_vector[i] * q.m_vector[i]; 
-    }
+            
+            Vector<T>(const usize size, T initial_val)
+                : Vector<T>(size)
+                {
+                    for(usize i = {}; i < m_total_elements; i++){
+                        m_vector[i] = initial_val;
+                    }
+                }
+            const_iterator cbegin() const { return m_vector; };
+            const_iterator cend() const { return m_vector + m_total_elements; }
+            const_iterator begin() const { return cbegin(); };
+            const_iterator end() const   { return cend();}
+            iterator begin() { return m_vector; };
+            iterator end()   { return m_vector + m_total_elements; };
 
-    return sum;
+            template<typename P>
+            friend openLA::Vector<P> operator+  (openLA::Vector<P> &lhs, 
+                                                 openLA::Vector<P> &rhs)
+                                                 noexcept
+            {
+                assert(lhs.m_total_elements == rhs.m_total_elements);
+                usize size{lhs.m_total_elements};
+
+                openLA::Vector<P> res(size);
+                for(usize i = 0; i < res.m_total_elements; i++){
+                    res.m_vector[i] = lhs.m_vector[i] + rhs.m_vector[i];
+                }
+
+                return res;
+            }
+            template<typename P>
+            friend openLA::Vector<P> operator-  (openLA::Vector<P> &lhs,
+                                                 openLA::Vector<P> &rhs)
+                                                 noexcept
+            {
+                assert(lhs.m_total_elements == rhs.m_total_elements);
+                usize size{lhs.m_total_elements};
+                openLA::Vector<P> res(size);
+                for(usize i = 0; i < res.m_total_elements; i++){
+                    res.m_vector[i] = lhs.m_vector[i] - rhs.m_vector[i];
+                }
+
+                return res;
+            }
+            template<typename P>
+            friend openLA::Vector<P> operator*  (openLA::Vector<P> &lhs, 
+                                                 openLA::Vector<P> &rhs)
+                                                 noexcept
+            {
+                assert(lhs.m_total_elements == rhs.m_total_elements);
+                usize size{lhs.m_total_elements};
+                openLA::Vector<P> res(size);
+                for(usize i = 0; i < res.m_total_elements; i++){
+                    res.m_vector[i] = lhs.m_vector[i] * rhs.m_vector[i];
+                }
+
+                return res;
+            }
+#if 0
+            template<typename P>
+            friend openLA::Vector<P> operator/  (openLA::Vector<P> &lhs, 
+                                                 openLA::Vector<P> &rhs)
+            {
+                assert(lhs.m_total_elements == rhs.m_total_elements);
+                usize size{lhs.m_total_elements};
+                openLA::Vector<P> res(size);
+                for(usize i = 0; i < res.m_total_elements; i++){
+                    #ifdef OPENLA_SECURITY_CHECK
+                    if(rhs.m_vector[i] == 0){
+                        throw std::runtime_error("ERROR: division by zero\n");
+                    }
+                    #endif
+                    res.m_vector[i] = lhs.m_vector[i] / rhs.m_vector[i];
+                }
+                
+                return res;
+            }
+#endif
+            template<typename P>
+            friend std::ostream& operator<<      (std::ostream &stream,
+                                                 const openLA::Vector<P> &vec)
+            {
+            
+                stream << "[ ";
+                for(usize i = 0; i < vec.m_total_elements; i++)
+                {
+                    stream << vec.m_vector[i] << ' ';
+                }
+                stream << " ]";
+                return stream;
+            }
+            T& operator[] (const usize index) noexcept
+            {
+                return m_vector[index];
+            }
+            openLA::Vector<T> resize(const usize new_size) noexcept;
+    };
+} // openLA
+template<typename T>
+openLA::Vector<T> openLA::Vector<T>::resize(const usize new_size) noexcept
+{
+    if(new_size == m_total_elements){
+        return *this;
+    }
+    openLA::Vector<T> new_vec(new_size);
+    for(usize i = 0; i < m_total_elements; i++)
+    {
+        new_vec[i] = m_vector[i];
+    }
+    *this = new_vec;
+    return *this;
 }
 #endif // VECTOR_HH
